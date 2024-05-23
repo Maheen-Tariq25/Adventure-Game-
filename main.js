665fd2fcba2d5f07a098e996f9ed8677ac0648b2
Maheen Tariq <164564115+Maheen-Tariq25@users.noreply.github.com>
2024-05-23 20:51:26 +0500

    my first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+    fuelIncrease() {
+        this.fuel = 100;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+}
+let player = await inquirer.prompt({
+    name: "name",
+    type: "input",
+    message: "Enter your name"
+});
+let opponent = "Skeleton";
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent);
+do {
+    let ask = await inquirer.prompt([{
+            name: "player",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Attack", "Health Portion", "Run for your life"]
+        }
+    ]);
+    if (ask.player === "Attack") {
+        let num = Math.floor(Math.random() * 2);
+        if (num > 0) {
+            p1.fuelDecrease();
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            if (p1.fuel <= 0) {
+                console.log(`\nYou Loose, Better Luck Next Time\n`);
+                process.exit();
+            }
+        }
+        if (num <= 0) {
+            o1.fuelDecrease();
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            if (o1.fuel <= 0) {
+                console.log(`\n*** You Win ***\n`);
+                process.exit();
+            }
+        }
+    }
+    else if (ask.player === "Health Portion") {
+        p1.fuelIncrease();
+        console.log(`You drink health portion your fuel is ${p1.fuel}`);
+    }
+    else if (ask.player === "Run for your life") {
+        console.log(`\nYou Loose, Better Luck Next Time \n`);
+        process.exit();
+    }
+} while (true);
